68c4cafb4d5aa3308456f5a4ec90da63441ef9b9
wh7rest <rikhteregor555@mail.ru>
2026-09-03 20:57:21 +0300

    feat(downloader):implement async tikwm client

--- downloader.py
+++ downloader.py
@@ -1,35 +1,44 @@
 import asyncio
-import logging
 from os import getenv
 
 import aiohttp
-from dotenv.main import load_dotenv
+from aiohttp import ClientTimeout
+from dotenv import load_dotenv
 
 load_dotenv()
 MY_API_KEY = getenv("MY_API_KEY")
 API_URL = "https://api.tikwmapi.com"
-headers = {"x-tikwmapi-key": str(MY_API_KEY)}
+
+if not MY_API_KEY:
+    raise ValueError("Critical error: environment variable MY_API_KEY is not set")
+headers = {"x-tikwmapi-key": MY_API_KEY}
 
 
-async def download_video(url: str) -> dict[str, str]:
+async def download_video(url: str) -> str:
     params = {"url": url, "hd": 1}
-    async with (
-        aiohttp.ClientSession() as session,
-        session.get(API_URL, params=params, headers=headers) as resp,
-    ):
-        if resp.status != 200:
-            error = {"code": resp.status, "error": await resp.text()}
-            print(error)
-            return error
-        else:
-            data = await resp.json()
-            print(data)
-            return data
+    try:
+        async with (
+            aiohttp.ClientSession(timeout=ClientTimeout(10)) as session,
+            session.get(API_URL, params=params, headers=headers) as resp,
+        ):
+            if resp.status != 200:
+                error = {"code": resp.status, "error": await resp.text()}
+                raise ValueError(f"Critical error: {error}")
 
+            else:
+                data = await resp.json()
+                if data["code"] != 0:
+                    raise ValueError(f"Error code {data['code']}, {data['msg']}")
 
-if __name__ == "__main__":
-    asyncio.run(download_video("https://vm.tiktok.com/ZN8YJG2hN/"))
+                video_link = data["data"].get("hdplay") or data["data"].get("play")
+                if not video_link:
+                    raise ValueError("Direct link to the video not found")
+                return video_link
 
+    except asyncio.TimeoutError:
+        raise ValueError(
+            "Request timed out: external API did not respond in 10 seconds"
+        )
 
-# все сработало, думаю что нельзя делать как я сделал в 11 строчке, подумать об этом потом
-# а вообще надо теперь делать парсинг, получать из ответа только title, url
+    except aiohttp.ClientError as err:
+        raise ValueError(f"Error: {err}")
